dcfb6d3a608ace922b5bc65e46b4e2262179a074
leeyang <lei.yang1123@gmail.com>
2023-10-10 12:47:19 -0700

    deploy client/server in the same endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const planetRouter = require('./routes/planets/route');
 const cors = require('cors');
+const path = require('path');
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetRouter);
 
